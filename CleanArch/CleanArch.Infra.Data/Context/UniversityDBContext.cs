698cb0f3bd844fd4425183cc9149e3bd7606257c
chougrani <chougranisouhail@gmail.com>
2019-12-21 17:39:31 +0100

    Add new DataBase UniversityDB. Update Startup class forn the new connection with the database

---
+++ CleanArch/CleanArch.Infra.Data/Context/UniversityDBContext.cs
@@ -0,0 +1,17 @@
+﻿using CleanArch.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.Infra.Data.Context
+{
+   public class UniversityDBContext:DbContext
+    {
+        public UniversityDBContext(DbContextOptions options ):base(options)
+        {
+
+        }
+        DbSet<Course> courses { get; set; } 
+    }
+}
