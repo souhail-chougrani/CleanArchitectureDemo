3dd20dc662a7e6ec4c8e77d2860e4481f5237c77
chougrani <chougranisouhail@gmail.com>
2019-12-21 18:34:26 +0100

    push

---
+++ CleanArch/CleanArch.Application/ViewModel/CourseViewModel.cs
@@ -0,0 +1,12 @@
+﻿using CleanArch.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.Application.ViewModel
+{
+   public class CourseViewModel
+    {
+        IEnumerable<Course> Courses { get; set; }
+    }
+}
---
+++ CleanArch/CleanArch.Domain/Repository/ICourseRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.Domain.Repository
+{
+   public interface ICourseRepository
+    {
+        IEnumerable<Course> GetCourses();
+    }
+
+}
